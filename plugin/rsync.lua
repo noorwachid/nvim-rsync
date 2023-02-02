14d1e397e9717e762a8f09def6e2854c8e1f6f86
Noor Wachid <wachid@fisip.net>
2023-02-02 14:11:58 +0700

    Allow multiple hop configurations

--- plugin/rsync.lua
+++ plugin/rsync.lua
@@ -1,64 +1,120 @@
-vim.api.nvim_create_user_command('RsyncUp', function()
+local function get_config()
     local cwd = vim.fn.getcwd()
-    local config_file = cwd .. '/.nvim/rsync.lua'
-    local config = {}
+    local path = cwd .. '/.nvim/rsync.lua'
+    local config = nil
 
-    if vim.fn.filereadable(config_file) then
-        local file = io.open(config_file)
+    if vim.fn.filereadable(path) then
+        local file = io.open(path)
 
         if (not file) then
-            return print('Config file is broken')
+            print('Config file is broken')
+            return false
         end
 
         local content = file:read('a')
-        local config_callback = load(content)
+        local callback = load(content)
 
-        if (not config_callback) then
-            return print('Config file has invalid syntax')
+        if (not callback) then
+            print('Config file has invalid syntax')
+            return false
         end
 
-        config = config_callback()
+        config = callback()
     end
 
-    if (not config.username) then
-        return print('Remote username is not defined')
+    return config
+end
+
+local function is_hop_valid(hop)
+    if not hop.username then
+        print('Remote username is not defined')
+        return false
     end
 
-    if (not config.host) then
-        return print('Remote host is not defined')
+    if not hop.host then
+        print('Remote host is not defined')
+        return false
     end
 
-    if (not config.path) then
-        return print('Remote path is not defined')
+    if not hop.path then
+        print('Remote path is not defined')
+        return false
     end
 
+    return true
+end
+
+local function execute_hop(table, hop_index)
+    if not table[hop_index] then
+        print('Hop with index ' .. hop_index .. ' is not defined')
+        return false
+    end
+
+    local hop = table[hop_index]
+
+    if not is_hop_valid(hop) then
+        return
+    end
+
+    local cwd = vim.fn.getcwd()
     local target_fp = vim.fn.expand('%:p')
     local target = target_fp:gsub(cwd, '')
 
     local from = cwd .. target
-    local to = config.path ..  target
+    local to = hop.path ..  target
 
-	print('syncing up ' .. target)
+    if hop_index == '' then
+        print('Syncing up ' .. target)
+    else
+        print('Syncing up [' .. hop_index .. '] ' .. target)
+    end
 
-    vim.fn.jobstart({'rsync', '-z', from, config.username .. '@' .. config.host .. ':' .. to}, {
-        stdout_buffered = true,
-        on_exit = function()
-            print('synced ' .. target)
-        end,
+    vim.fn.jobstart({'rsync', '-z', from, hop.username .. '@' .. hop.host .. ':' .. to}, {
+      on_exit = function()
+          print('synced ' .. target)
+      end,
 
-        on_stdout = function(_, data)
-            if data and data[1] ~= '' then
-                print('synced ' .. target)
-                table.concat(data, '\n')
-            end
-        end,
+      stdout_buffered = true,
+      on_stdout = function(_, data)
+          if data and data[1] ~= '' then
+              print('synced ' .. target)
+              table.concat(data, '\n')
+          end
+      end,
 
-        stderr_buffered = true,
-        on_stderr = function(_, data)
-            if data and data[1] ~= '' then
-                print('failed to sync ' .. target)
-                table.concat(data, '\n')
-            end
-        end
+      stderr_buffered = true,
+      on_stderr = function(_, data)
+          if data and data[1] ~= '' then
+              print('failed to sync ' .. target)
+              table.concat(data, '\n')
+          end
+      end
     })
-end, { desc = 'remote sync upstream' })
+end
+
+vim.api.nvim_create_user_command('RsyncUp', function(context)
+    local config = get_config()
+
+    if not config then
+        return
+    end
+
+    if not config.table then
+        if is_hop_valid(config) then
+            return execute_hop({ [''] = config }, '')
+        end
+
+        return print('Config file is not valid [HOP table is not defined]')
+    end
+
+    if context.args == '' then
+        if not config.default then
+            return print('Config file does not specify default hop index')
+        end
+
+        return execute_hop(config.table, config.default)
+    end
+
+    return execute_hop(config.table, context.args)
+
+end, { desc = 'remote sync upstream', nargs = '?' })
